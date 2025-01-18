65a81e7ba224df7d3717364b1a72b8ce5f44113a
pavan s <pavan.s@pentagonspace.in>
2025-01-18 20:49:43 +0530

    JSX  rules and adding image in react

--- src/App.js
+++ src/App.js
@@ -3,31 +3,19 @@ import React from'react'
 //examples related to jsx
 
 function App() {
-  let element=<h1>Welcome to React JS</h1>   
+  let ImageUrl="https://www.motortrend.com/uploads/2022/04/2023-Jaguar-F-Type-R-Coupe_1-2.jpg"
+ let Fun=()=>{
+  alert("hello how are you")
+ }   
   return (
-        //example related to jsx in lower case
-    //   {/* <H1>hello how are you</H1> */}
-
-    //example related to jsx must have single
-    // <section>
-    // <div> 
-    //   <h1>This my first react code</h1>
-    //   {/* {element}
-    //   30+90
-    //   {30+90} */}
-
-    //   <p>Hello how are you welcome react class</p>
-    // </div>
-    // <div>
-    //   <h1>welcome</h1>
-    // </div>
-    // </section>
-    
-//example related to react fragment
-    <React.Fragment>
-       <h1>welcome</h1>
-       <p>Hello how are you welcome react class</p>
-    </React.Fragment>
+       <>
+       <img src='https://www.hdwallpapers.in/download/2024_lamborghini_revuelto_car_4k_5k_hd_cars-5120x2880.jpg'
+       height="200" width="200"/>
+       <img src='Car1.webp' alt='no image'height="200" width="200"/>
+       <img src={ImageUrl} alt='no image'height="200" width="200"/>
+       {/* <p className="one">welcome to js class</p>
+       <button onClick={Fun}>Click here</button> */}
+       </>
   );
 }
 
